23001b909c4ee697ff193acb415a0f8a858cf03e
micaelpimentel <micael.pimentel@icomp.ufam.edu.br>
2021-08-19 23:01:11 +0000

    Hello world

--- app.py
+++ app.py
@@ -1,1 +1,8 @@
-print("hello world")
+import sqlite3
+from flask import Flask, request, session, g, redirect, abort, render template
+
+app = Flask(__name__)
+
+@app.route('/hello')
+def pagina_inicial():
+    return "Hello world, caraiu, pyrra!!"
