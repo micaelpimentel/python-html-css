e1a7613a5cf3aac347cf8c271c7b627f56dccf98
micaelpimentel <micael.pimentel@icomp.ufam.edu.br>
2021-08-19 23:35:54 +0000

    Added requests handlers

--- app.py
+++ app.py
@@ -1,7 +1,23 @@
 import sqlite3
 from flask import Flask, request, session, g, redirect, abort, render_template, flash
 
+# configuração
+DATABASE = "blog.db"
+SECRECT_KEY = 'pudim'
+
 app = Flask(__name__)
+app.config.from_object(__name__)
+
+def conectar_bd():
+    return sqlite3.connect(app.config['DATABASE'])
+
+@app.before_request
+def antes_requisicao():
+    g.bd = conectar_bd()
+
+@app.teardown_request
+def depois_request(exc):
+    g.bd.close()
 
 @app.route('/hello')
 def pagina_inicial():
